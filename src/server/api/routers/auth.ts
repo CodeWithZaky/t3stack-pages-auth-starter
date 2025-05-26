4986e86d9856bec736535ab920f417bc8c540641
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2025-05-26 14:40:33 +0700

    add auth router with user register

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,14 +1,13 @@
-import { Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import bcrypt from "bcrypt";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const userRegisterRoute = createTRPCRouter({
-  userRegister: publicProcedure
+export const authRouter = createTRPCRouter({
+  register: publicProcedure
     .input(
       z.object({
-        email: z.string().email(),
+        email: z.string().min(1).email(),
         name: z.string().min(1),
         password: z.string().min(8),
       }),
@@ -16,7 +15,6 @@ export const userRegisterRoute = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       const { name, email, password } = input;
       try {
-        // Cek apakah email sudah terdaftar
         const existingUser = await ctx.db.user.findUnique({
           where: { email },
         });
@@ -26,9 +24,9 @@ export const userRegisterRoute = createTRPCRouter({
             message: "Email already exists. Please use a different email.",
           });
         }
-        // Hash password
+
         const hashedPassword = await bcrypt.hash(password, 10);
-        // Buat user baru
+
         const user = await ctx.db.user.create({
           data: {
             name,
@@ -36,23 +34,13 @@ export const userRegisterRoute = createTRPCRouter({
             password: hashedPassword,
           },
         });
+
         return {
           status: "success",
           message: "User registered successfully",
           user,
         };
       } catch (error) {
-        // Handling Prisma unique constraint error (misalnya email duplikat)
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-          if (error.code === "P2002") {
-            throw new TRPCError({
-              code: "CONFLICT",
-              message: "Email already exists. Please use a different email.",
-            });
-          }
-        }
-        // Handling error umum yang tidak terduga
-        console.error("Unexpected error during registration:", error);
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message:
